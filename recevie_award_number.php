4bfbe33de78e770daeee81214a74d525e0e9d8f5
kuan-git <2857xxx9923>
2019-11-15 16:27:16 +0800

    1115s

---
+++ recevie_award_number.php
@@ -0,0 +1,26 @@
+<?php
+
+$ssp = $_POST['awa1'];
+$sp = $_POST['awa2'];
+$first=$_POST['awa3'];
+$cr6=$_POST['awa4'];
+
+echo $ssp ;
+echo $sp ;
+print_r($first);
+print_r($cr6);
+
+
+;
+;
+
+
+
+
+
+
+
+
+
+
+?>
